e7cf6c3348096c648928fc90deb47a945857eb92
Toshiaki Maki <tmaki@pivotal.io>
2019-11-28 01:27:33 +0900

    prepare-release.sh

---
+++ prepare-release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+VERSION=$1
+
+if [ "${VERSION}" = "" ];then
+  echo "usage: $0 VERSION"
+  exit 1
+fi
+
+set -x
+
+mvn versions:set -DnewVersion=${VERSION} -DgenerateBackupPoms=false
+git commit -m "Release ${VERSION}" pom.xml
+git tag ${VERSION}
+./build-all-binaries.sh
+set +x
+echo "Run: git push origin ${VERSION}"
